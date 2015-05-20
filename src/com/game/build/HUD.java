1e7a951a83381491bfe99c844d4df23f6fca9d07
aminosora <atisamurai@gmail.com>
2015-05-20 11:53:01 +0400

    A few little bug fixes

--- src/com/game/build/HUD.java
+++ src/com/game/build/HUD.java
@@ -5,16 +5,16 @@ import java.awt.Graphics;
 
 public class HUD {
 	
-	public static int HEALTH = 100;
-	private int greenValue = 255;
+	public static float HEALTH = 100;
+	private float greenValue = 255f;
 	
 	private int score = 0;
 	private int level = 1;
 	
 	public void tick(){
 		
-		HEALTH = (int) Game.clamp(HEALTH, 0, 100);
-		greenValue = (int) Game.clamp(greenValue, 0, 255);
+		HEALTH = Game.clamp(HEALTH, 0, 100);
+		greenValue = Game.clamp(greenValue, 0, 255);
 		
 		greenValue = HEALTH * 2;
 		
@@ -25,8 +25,8 @@ public class HUD {
 	public void render(Graphics g){
 		g.setColor(Color.gray);
 		g.fillRect(15, 15, 200, 32);
-		g.setColor(new Color(75, greenValue, 0));
-		g.fillRect(15, 15, HEALTH * 2, 32);
+		g.setColor(new Color(75, (int) greenValue, 0));
+		g.fillRect(15, 15, (int) (HEALTH * 2), 32);
 		g.setColor(Color.white);
 		g.drawRect(15, 15, 200, 32);
 		
